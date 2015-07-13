a871355e6ff6448ed78862dfdc0b697ee7895136
xian205 <285087066@qq.com>
2015-07-13 16:59:47 +0800

    bayes

--- chapter04/bayes.py
+++ chapter04/bayes.py
@@ -1,3 +1,5 @@
+from numpy import *
+from numpy import array
 def loadDataSet():
 	postingList=[['my', 'dog', 'has', 'flea', \
 		'problems', 'help', 'please'],
@@ -9,7 +11,7 @@ def loadDataSet():
 		['mr', 'licks', 'ate', 'my', 'steak', 'how',\
 		'to', 'stop', 'him'],
 		['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
-	classVec = [0,1,0,1,1]
+	classVec = [0,1,0,1,0,1]
 	return postingList,classVec
 def createVocabList(dataSet):
 	vocabSet = set([])
@@ -26,4 +28,38 @@ def setOfWords2Vec(vocabList,inputSet):
 def trainNB0(trainMatrix,trainCategory):
 	numTrainDocs = len(trainMatrix)
 	numWords = len(trainMatrix[0])
-	
+	pAbustive = sum(trainCategory)/float(numTrainDocs)
+	p0Num = ones(numWords)
+	p1Num = ones(numWords)
+	p0Denom = 2.0
+	p1Denom = 2.0
+	for i in range(numTrainDocs):
+		if trainCategory[i] == 1:
+			p1Num += trainMatrix[i]
+			p1Denom += sum(trainMatrix[i])
+		else:
+			p0Num += trainMatrix[i]
+			p0Denom += sum(trainMatrix[i])
+	p1Vect = log(p1Num/p1Denom)
+	p0Vect = log(p0Num/p0Denom)
+	return p0Vect,p1Vect,pAbustive
+def classifyNB(vec2Classify,p0Vec,p1Vec,pClass1):
+	p1 = sum(vec2Classify * p1Vec) + log(pClass1)
+	p0 = sum(vec2Classify * p0Vec) + log(1.0 - pClass1)
+	if p1 > p0:
+		return 1
+	else:
+		return 0 
+def testingNB():
+	listOPosts,listClasses = loadDataSet()
+	myVocabList = createVocabList(listOPosts)
+	trainMat=[]
+	for postinDoc in listOPosts:
+		trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+	p0V,p1V,pAb= trainNB0(array(trainMat),array(listClasses))
+	thisEntry = ['love','my','dalmation']
+	thisDoc = array(setOfWords2Vec(myVocabList,thisEntry))
+	print(thisEntry,'classified as:',classifyNB(thisEntry,p0V,p1V,pAb))
+	thisEntry = ['stupid','garbage']
+	thisDoc = array(setOfWords2Vec(myVocabList,thisEntry))
+	print(thisEntry,'classified as:',classifyNB(thisEntry,p0V,p1V,pAb))	
