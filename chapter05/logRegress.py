3648dd9013221e0ccc0cb1146c1048a89aade831
xian205 <285087066@qq.com>
2015-07-15 16:08:06 +0800

    doneLR

--- chapter05/logRegress.py
+++ chapter05/logRegress.py
@@ -56,3 +56,50 @@ def stocGradAscent(dataMatIn,classLabels):
 		error = classLabels[i] - h
 		weights = weights + alpha*error*dataMatIn[i]
 	return weights
+def stocGradAscent1(dataMatIn,classLabels,numIter=150):
+	m,n = shape(dataMatIn)
+	weights = ones(n)
+	for j in range(numIter):
+		for i in range(n):
+			dataIndex = list(range(m))
+			alpha = 4/(1.0+j+i) +0.01
+			randIndex = int(random.uniform(0,len(dataIndex)))
+			h = sigmoid(sum(dataMatIn[randIndex]*weights))
+			error = classLabels[randIndex] - h
+			weights = weights + alpha*error*dataMatIn[randIndex]
+			del(dataIndex[randIndex])
+	return weights
+def classifyVector(inX,weights):
+	prob = sigmoid(sum(inX*weights))
+	if prob > 0.5:	return 1.0
+	else:	return 0.0
+def colicTest():
+	frTrain = open('horseColicTrain.txt')
+	frTest = open('horseColicTest.txt')
+	trainingSet = []; trainingLabels = []
+	for line in frTrain.readlines():
+		currLine = line.strip().split('\t')
+		lineArr = []
+		for i in range(21):
+			lineArr.append(float(currLine[i]))
+		trainingSet.append(lineArr)
+		trainingLabels.append(float(currLine[21]))
+	trainWeights = stocGradAscent1(array(trainingSet),trainingLabels,500)
+	errorCount = 0; numTestVec = 0.0
+	for line in frTest.readlines():
+		numTestVec += 1.0
+		currLine = line.strip().split('\t')
+		lineArr = []
+		for i in range(21):
+			lineArr.append(float(currLine[i]))
+		if int(classifyVector(array(lineArr),trainWeights)) != int(currLine[21]):
+			errorCount += 1
+		errorRate = float(errorCount)/numTestVec
+		print("the error rate is : %f" % errorRate)
+	return errorRate
+def multiTest():
+	numTests = 10; errorSum = 0.0
+	for k in range(numTests):
+		errorSum += colicTest()
+	print("after %d iterations th average error rate is : %f" % (numTests,errorSum/float(numTests)))
+
